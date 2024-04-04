4fb7429996bb23d4a0eb497c4f18220938508f3e
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-04-04 22:44:15 +0000

    Aleatoria-Card

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const paLo = [`♠`, `♦`, `♣`, `♥`];
+  const numEro = [`A`, 2, 3, 4, 5, 6, 7, 8, 9, `J`, `Q`, `K`];
+
+  console.log(numEro);
+
+  function getRandomNum(numeros) {
+    return numeros[Math.floor(Math.random() * numeros.length)];
+  }
+  const barajaAleatoria = getRandomNum(numEro);
+  console.log(barajaAleatoria);
+
+  const paLoAleatorio = getRandomNum(paLo);
+  console.log(paLoAleatorio);
+
+  document.querySelector("#paloA").textContent = paLoAleatorio;
+  document.querySelector("#paloB").textContent = paLoAleatorio;
+
+  if (paLoAleatorio === `♠` || paLoAleatorio === `♣`) {
+    const cambioColor = document.querySelector("div");
+    cambioColor.style.color = "black";
+  } else {
+    const cambioColor2 = document.querySelector("div");
+    cambioColor2.style.color = "red";
+  }
+
+  const cartaLoca = document.querySelector("#numero").textContent;
+
+  document.querySelector("#numero").textContent = barajaAleatoria;
 };
